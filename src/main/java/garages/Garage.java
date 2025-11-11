af32dd19e972a94fad56f15d3ad1755bd4437dd0
Rémi Bastide <Remi.Bastide@irit.fr>
2025-11-11 11:21:30 +0100

    comments

--- src/main/java/garages/Garage.java
+++ src/main/java/garages/Garage.java
@@ -22,33 +22,4 @@ public class Garage {
 	@Setter // Génère le setter pour ce champ
 	private String name;
 
-	/**
-	 * Crée un nouveau garage avec le nom spécifié.
-	 * 
-	 * @param name le nom du garage (ne peut pas être null)
-	 * @throws NullPointerException si le nom est null
-	 */
-	// Le constructeur est généré par @RequiredArgsConstructor
-
-	/**
-	 * Retourne le nom du garage.
-	 * 
-	 * @return le nom du garage
-	 */
-	// La méthode getName() est générée par @Getter
-
-	/**
-	 * Modifie le nom du garage.
-	 * 
-	 * @param name le nouveau nom du garage (ne peut pas être null)
-	 * @throws NullPointerException si le nouveau nom est null
-	 */
-	// La méthode setName() est générée par @Setter
-
-	/**
-	 * Retourne une représentation sous forme de chaîne de caractères du garage.
-	 * 
-	 * @return une chaîne de caractères représentant le garage, incluant son nom
-	 */
-	// La méthode toString() est générée par @ToString
 }
